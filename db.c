91eafdf1683f16a7e623217728fa4db4f60ecbfb
Erik Hollembeak <erik.hollembeak@gmail.com>
2018-02-16 01:34:00 -0800

    Add trivial ability to prepare and execute SQL statements
    Only recognizes “select” and “insert”, and only in the lowercase.
    Execution consists of recognizing the type of statement that’s been
    prepared and stating as much

--- db.c
+++ db.c
@@ -38,6 +38,43 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer)
     }
 }
 
+enum PrepareResult_t {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED
+};
+
+typedef enum PrepareResult_t PrepareResult;
+
+enum StatementType_t {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+};
+
+typedef enum StatementType_t StatementType;
+
+struct Statement_t {
+    StatementType type;
+};
+
+typedef struct Statement_t Statement;
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
+{
+    if(strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+
+    if(strncmp(input_buffer->buffer, "select", 6) == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED;
+}
+
 void print_prompt()
 {
     printf("db > ");
@@ -59,6 +96,18 @@ void read_input(InputBuffer* input_buffer)
     input_buffer->buffer[bytes_read - 1] = 0;
 }
 
+void execute_statement(Statement* statement)
+{
+    switch(statement->type)
+    {
+        case STATEMENT_INSERT:
+            printf("This is where we would do an insert.\n");
+            break;
+        case STATEMENT_SELECT:
+            printf("This is where we would do a select.\n");
+            break;
+    }
+}
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
     while(true) {
@@ -76,5 +125,20 @@ int main(int argc, char* argv[]) {
                     continue;
             }
         }
+
+        Statement statement;
+
+        switch(prepare_statement(input_buffer, &statement))
+        {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED:
+                printf("Unrecognized keyword at start of '%s'\n",
+                                                      input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed\n");
     }
 }
